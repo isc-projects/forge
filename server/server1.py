c3728bf36cf1185bd0f58a3204cd761c89f83d43
Rafał Jankowski <jankowsr@oceanic.wsisiz.edu.pl>
2012-11-11 15:02:09 +0100

    fixed indentation + typo

--- server/server1.py
+++ server/server1.py
@@ -8,7 +8,7 @@ def option_parser():
     desc="""Sending version number of DHCP server. PORT number need to be the same in client and server. Keep this working in testing time.
     """
     parser = optparse.OptionParser(description=desc)
-    parser.add_option('-p', '--port', help='on witch port listetnig for question, default 9999', dest='port', type='int', default=9999, action='store')
+    parser.add_option('-p', '--port', help='on witch port listening for question, default 9999', dest='port', type='int', default=9999, action='store')
     (opts, args) = parser.parse_args()
 
     detect_version(opts.port)
@@ -25,12 +25,12 @@ def detect_version(port):
     try:
         s.listen(5)
         while True:
-           c, addr = s.accept()
-           print 'Got connection from', addr
-           run=subprocess.Popen(['dhcpd', '--version'],stderr=subprocess.PIPE)
-           version= str(run.communicate()[1])[:-1]
-           c.send(version)
-           c.close()
+            c, addr = s.accept()
+            print 'Got connection from', addr
+            run=subprocess.Popen(['dhcpd', '--version'],stderr=subprocess.PIPE)
+            version= str(run.communicate()[1])[:-1]
+            c.send(version)
+            c.close()
     except KeyboardInterrupt:
         print 'Program Interrupted by user'
         sys.exit()
