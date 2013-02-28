87e8d6434e74c0a8f512d74dfacebad153db8084
wwencel <wlodek.wencel@gmail.com>
2013-03-01 00:52:49 +0100

    tagowanie...

--- lettuce/run_test.py
+++ lettuce/run_test.py
@@ -67,7 +67,36 @@ def option_parser():
 
     runner = Runner(base_path,
                     verbosity=VERBOSITY,
-                    tags=tag)#TUTAJ PROBLEM, wybierac testy mozna wedlugo tagow, podobno...
+                    tags=tag)
+    '''
+    Aby tagowanie dzialalo (?) trzeba tagowac przez @ mniej wiecej tak:
+
+        @v6
+        Feature: DHCPv6 custom options
+            This is a simple DHCPv6 options validation. Its purpose is to check if
+            requested options are assigned properly.
+        
+            Scenario: v6.options.preference
+                # Checks that server is able to serve sntp-servers option to clients.
+        
+                Test Setup:
+                Server is configured with 3000::/64 subnet with 3000::1-3000::ff pool.
+                Server is configured with custom option foo/100 with type uint8 and value 123.
+                Server is started.
+        
+            Test Procedure:
+            Client requests option 100.
+            Client sends SOLICIT message and expect ADVERTISE response.
+        
+            Pass Criteria:
+            Server MUST respond with advertise message.
+            Response MUST include option 100.
+            Response option 100 MUST contain uint8 123.
+        
+            References: v6.user-options, v6.oro, RFC3315 section 22.8
+        
+            Tags: v6 options preference automated
+    '''
     result = runner.run()
     
 def main():
