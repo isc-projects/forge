a0da0965d1e12ed3efd8ebcb3312665bda7d7b66
Marcin Siodelski <marcin@isc.org>
2013-05-06 11:31:10 +0200

    Do not use the full name pattern when killing hanging b10 process.

--- lettuce/features/terrain.py
+++ lettuce/features/terrain.py
@@ -30,7 +30,7 @@ def kill_bind10(host):
     Kill any running bind10 instance
     """
     get_common_logger().debug("--- Killing all running Bind instances")
-    cmd = 'pkill -f b10-*; sleep 5'
+    cmd = 'pkill b10-*; sleep 5'
     with settings(host_string=host, user=MGMT_USERNAME, password=MGMT_PASSWORD):
         with settings(warn_only=True):
             sudo(cmd, pty=True)
