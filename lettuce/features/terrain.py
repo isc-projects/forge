2585b7d2bf992c7e31cd3b6984772221609ec106
wlodek <wlodek@isc.org>
2014-03-20 20:48:37 -0400

    new tests for requesting address process

--- lettuce/features/terrain.py
+++ lettuce/features/terrain.py
@@ -34,58 +34,58 @@ import subprocess
 import sys
 import time
 
-add_option_v6 = {'client_id' : True,
-              'preference' : False,
-              'time' : False,
-              'relay_msg' : False,
-              'server_uni' : False,
-              'status_code' : False,
-              'rapid_commit' : False,
-              'interface_id' : False,
-              'reconfig' : False,
-              'option_request' : False,
-              'reconfig_accept' : False,
-              'server_id' : False,
-              'wrong_client_id' : False,
-              'wrong_server_id' : False,
-              'IA_NA': True,
-              'IA_TA': False,
-              'IA_PD': False,
-              'IA_Prefix': False,
-              'IA_Address': False,
-              'vendor_class': False,
-              'vendor_specific_info': False
-              }
+add_option_v6 = {'client_id': True,
+                 'preference': False,
+                 'time': False,
+                 'relay_msg': False,
+                 'server_uni': False,
+                 'status_code': False,
+                 'rapid_commit': False,
+                 'interface_id': False,
+                 'reconfig': False,
+                 'option_request': False,
+                 'reconfig_accept': False,
+                 'server_id': False,
+                 'wrong_client_id': False,
+                 'wrong_server_id': False,
+                 'IA_NA': True,
+                 'IA_TA': False,
+                 'IA_PD': False,
+                 'IA_Prefix': False,
+                 'IA_Address': False,
+                 'vendor_class': False,
+                 'vendor_specific_info': False
+                 }
 
-values_v6 = {"T1": 0, # IA_NA IA_PD
-          "T2": 0, # IA_NA IA_PD
-          "address": "::",
-          "prefix": "::",
-          "plen": 0,  # prefix; plz remember, to add prefix and prefix length!
-          "preflft" : 0, # IA_Address IA_Prefix
-          "validlft" : 0, # IA_Address IA_Prefix
-          "enterprisenum": 0, # vendor 
-          "linkaddr": "3000::ffff", # relay
-          "peeraddr": "2000::1", # relay
-          "ifaceid": "15", # relay
-          "DUID": None          
-          }
+values_v6 = {"T1": 0,  # IA_NA IA_PD
+             "T2": 0,  # IA_NA IA_PD
+             "address": "::",
+             "prefix": "::",
+             "plen": 0,  # prefix; plz remember, to add prefix and prefix length!
+             "preflft": 0,  # IA_Address IA_Prefix
+             "validlft": 0,  # IA_Address IA_Prefix
+             "enterprisenum": 0,  # vendor
+             "linkaddr": "3000::ffff",  # relay
+             "peeraddr": "2000::1",  # relay
+             "ifaceid": "15",  # relay
+             "DUID": None
+             }
 
 # times values, plz do not change this.
 # there is a test step to do this
 server_times_v6 = {"renew-timer": 1000,
-                "rebind-timer": 2000,
-                "preferred-lifetime": 3000,
-                "valid-lifetime": 4000,
-                "rapid-commit": False # yes that little odd, but let us keep it that way, 
-                                      # rapid-commit it's only option that is used 
-                                      # only in server configuration
-                }
+                   "rebind-timer": 2000,
+                   "preferred-lifetime": 3000,
+                   "valid-lifetime": 4000,
+                   "rapid-commit": False  # yes that little odd, but let us keep it that way,
+                   # rapid-commit it's only option that is used
+                   # only in server configuration
+                   }
 
 server_times_v4 = {"renew-timer": 1000,
                    "rebind-timer": 2000,
                    "valid-lifetime": 4000,
-                   } 
+                   }
 
 values_v4 = {"source_IP": "0.0.0.0",
              "dstination_IP": "255.255.255.255",
@@ -96,8 +96,7 @@ values_v4 = {"source_IP": "0.0.0.0",
              "chaddr": None
              }
 
-add_option_v4 = {
-                 "vendor_class_id": False,
+add_option_v4 = {"vendor_class_id": False,
                  "client_id": True
                  }
 
@@ -111,37 +110,45 @@ def set_values():
     else:
         world.cfg["values"] = values_v4.copy()
         world.cfg["server_times"] = server_times_v4.copy()
-        
+
+
 def set_options():
     if PROTO == "v6":
         world.cfg["add_option"] = add_option_v6.copy()
     else:
         world.cfg["add_option"] = add_option_v4.copy()
-    
+
+
 def add_result_to_raport(info):
     world.result.append(info)
 
-def client_id (mac):
-    world.cfg["cli_duid"] = DUID_LLT(timeval = int(time.time()), lladdr = mac )
 
-def ia_id ():
-    world.cfg["ia_id"] = randint(1,99999)
+def client_id(mac):
+    world.cfg["cli_duid"] = DUID_LLT(timeval=int(time.time()), lladdr=mac)
 
-def ia_pd ():
-    world.cfg["ia_pd"] = randint(1,99999)
+
+def ia_id():
+    world.cfg["ia_id"] = randint(1, 99999)
+
+
+def ia_pd():
+    world.cfg["ia_pd"] = randint(1, 99999)
+
 
 def multiprotocol_initialize():
     pass
 
+
 def v4_initialize():
     # Setup scapy for v4
     #conf.iface = IFACE
-    conf.checkIPaddr = False # DHCPv4 is sent from 0.0.0.0, so response matching may confuse scapy
+    conf.checkIPaddr = False  # DHCPv4 is sent from 0.0.0.0, so response matching may confuse scapy
     world.cfg["srv4_addr"] = SRV4_ADDR
     world.cfg["rel4_addr"] = REL4_ADDR
     world.cfg["giaddr4"] = GIADDR4
     world.cfg["space"] = "dhcp4"
     world.cfg["PACKET_WAIT_INTERVAL"] = PACKET_WAIT_INTERVAL
+
 
 def v6_initialize():
     # RFC 3315 define two addresess:
@@ -155,7 +162,8 @@ def v6_initialize():
     # Setup scapy for v6
     conf.iface6 = IFACE
     conf.use_pcap = True
-    
+
+
 @before.all
 def server_start():
     """
@@ -163,41 +171,44 @@ def server_start():
     """
     # clear tests results
     from shutil import rmtree
+
     if os.path.exists('tests_results'):
         rmtree('tests_results')
     os.makedirs('tests_results')
     if not os.path.exists('tests_results_archive') and AUTO_ARCHIVE:
         os.makedirs('tests_results_archive')
-        
+
     world.result = []
-    
+
     # Initialize the common logger. The instance of this logger can
     # be instantiated by get_common_logger()
     logger_initialize(LOGLEVEL)
 
     if (SERVER_TYPE in ['kea', 'kea4', 'kea6']):
         get_common_logger().debug("Starting Bind:")
-        
+
         try:
             # Make sure there is noo garbage instance of bind10 running.
             kill_bind10()
             start_bind10()
             get_common_logger().debug("Bind10 successfully started")
-        except :
+        except:
             get_common_logger().error("Bind10 start failed\n\nSomething go wrong with connection\n\
                                         Please make sure it's configured properly\nIP destination \
                                         address: %s\nLocal Mac address: %s\nNetwork interface: %s"
-                                         %(MGMT_ADDRESS, CLI_MAC, IFACE))
+                                      % (MGMT_ADDRESS, CLI_MAC, IFACE))
             sys.exit()
     elif SERVER_TYPE in ["isc_dhcp6", "dibbler"]:
         # TODO: import only one function
-        stop = importlib.import_module("serversupport.%s.functions"  % (SERVER_TYPE))
+        stop = importlib.import_module("serversupport.%s.functions" % (SERVER_TYPE))
         stop.stop_srv()
 
     else:
-        get_common_logger().error("Server "+SERVER_TYPE+" not implemented yet")
-        
-    #If relay is used routing needs to be reconfigured on DUT
+        get_common_logger().error("Server " + SERVER_TYPE + " not implemented yet")
+
+        #If relay is used routing needs to be reconfigured on DUT
+
+
 #     try:
 #         if REL4_ADDR and (SERVER_TYPE  == 'kea4'):
 #             assert False, "we don't support ip v4 yet"
@@ -208,13 +219,12 @@ def server_start():
 
 @before.each_scenario
 def initialize(scenario):
-
     world.climsg = []  # Message(s) to be sent
-    world.cliopts = [] # Option(s) to be included in the next message sent
+    world.cliopts = []  # Option(s) to be included in the next message sent
     world.srvmsg = []  # Server's response(s)
-    world.savedmsg = [] # Saved option(s)
-    world.define = [] # temporary define variables
-    
+    world.savedmsg = []  # Saved option(s)
+    world.define = []  # temporary define variables
+
     world.proto = PROTO
     world.oro = None
     world.vendor = []
@@ -226,24 +236,24 @@ def initialize(scenario):
     world.cfg["server_type"] = SERVER_TYPE
 
     world.cfg["cfg_file"] = "server.cfg"
-    world.cfg["conf"] = "" # Just empty config for now
-    
-    dir_name = str(scenario.name).replace(".","_")
-    world.cfg["dir_name"] = 'tests_results/'+dir_name 
-    
+    world.cfg["conf"] = ""  # Just empty config for now
+
+    dir_name = str(scenario.name).replace(".", "_")
+    world.cfg["dir_name"] = 'tests_results/' + dir_name
+
     world.cfg["subnet"] = ""
-   
+
     world.name = scenario.name
 
     # Setup DUID for DHCPv6 (and also for DHCPv4, see RFC4361)
     if (not hasattr(world.cfg, "cli_duid")):
-        client_id (CLI_MAC)
-        
+        client_id(CLI_MAC)
+
     if (not hasattr(world.cfg, "ia_id")):
-        ia_id ()
-        
+        ia_id()
+
     if (not hasattr(world.cfg, "ia_pd")):
-        ia_pd ()
+        ia_pd()
 
     # set couple things depending on used IP version:
     # IPv6:
@@ -253,32 +263,32 @@ def initialize(scenario):
     if world.proto == "v4":
         v4_initialize()
 
-    set_values() 
+    set_values()
     set_options()
 
     if TCPDUMP:
         # to create separate files for each test we need:
         # create new directory for that test:
-        
+
         if not os.path.exists(world.cfg["dir_name"]):
             os.makedirs(world.cfg["dir_name"])
         # create new file for capture
-        if not os.path.exists(world.cfg["dir_name"]+'/capture.pcap'):
-            tmp = open(world.cfg["dir_name"]+'/capture.pcap', 'w+')
+        if not os.path.exists(world.cfg["dir_name"] + '/capture.pcap'):
+            tmp = open(world.cfg["dir_name"] + '/capture.pcap', 'w+')
             tmp.close()
         # also IP version for tcpdump
         type = 'ip'
-        
+
         if PROTO == "v6":
-            type = type +'6'
-        cmd = TCPDUMP_INSTALL_DIR+'tcpdump'
-        args = [cmd, type, "-i", world.cfg["iface"], "-U", "-w", world.cfg["dir_name"]+"/capture.pcap", "-s", str(65535)]
+            type = type + '6'
+        cmd = TCPDUMP_INSTALL_DIR + 'tcpdump'
+        args = [cmd, type, "-i", world.cfg["iface"], "-U", "-w",
+                world.cfg["dir_name"] + "/capture.pcap", "-s", str(65535)]
         get_common_logger().debug("Running tcpdump: ")
         get_common_logger().debug(args)
         # TODO: hide stdout, log it in debug mode
         subprocess.Popen(args)
 
-#initialize()
 
 @before.outline
 def outline_before(scenario, number, step, failed):
@@ -290,7 +300,8 @@ def outline_before(scenario, number, step, failed):
         failed - reason of failure
     For more info please read UserHelp - Outline Scenarios
     """
-    initialize(None)#we need to initialize all
+    initialize(None)  # we need to initialize all
+
 
 @after.outline
 def outline_result(scenario, number, step, failed):
@@ -306,33 +317,37 @@ def outline_result(scenario, number, step, failed):
         result = 'False'
     else:
         result = 'True'
-    info = str(scenario.name)+str(step)+'\n'+ result
+    info = str(scenario.name) + str(step) + '\n' + result
     add_result_to_raport(info)
+
 
 @after.each_step
 def cleanup_option(step):
     #set_options ()
     pass
-    
+
+
 @after.each_scenario
 def cleanup(scenario):
     """
     Global cleanup for each scenario. Implemented within tests by "Server is started."
     """
-    info = str(scenario.name) +'\n'+ str(scenario.failed)
+    info = str(scenario.name) + '\n' + str(scenario.failed)
     if 'outline' not in info:
         add_result_to_raport(info)
 
     if TCPDUMP:
         args = ["killall tcpdump"]
-        subprocess.call(args, shell = True)
+        subprocess.call(args, shell=True)
         # TODO: log output in debug mode
 
     # copy log file from remote server:
     if SAVE_BIND_LOGS:
-        fabric_download_file('log_file',world.cfg["dir_name"]+'/log_file')
-    if SAVE_LEASES and SERVER_TYPE not in ['kea', 'kea4', 'kea6']:        
-        fabric_download_file(world.cfg['leases'], world.cfg["dir_name"]+'/dhcpd6.leases')
+        fabric_download_file('log_file', world.cfg["dir_name"] + '/log_file')
+    if SAVE_LEASES and SERVER_TYPE not in ['kea', 'kea4', 'kea6']:
+        fabric_download_file(world.cfg['leases'], world.cfg["dir_name"] + '/dhcpd6.leases')
+
+
 @after.all
 def say_goodbye(total):
     """
@@ -343,30 +358,29 @@ def say_goodbye(total):
         total.scenarios_ran
     ))
     if HISTORY:
-        result = open ('result','w')
+        result = open('result', 'w')
         for item in world.result:
-            result.write(str(item)+'\n')
+            result.write(str(item) + '\n')
         result.close()
-        
+
     if SERVER_TYPE in ['kea', 'kea4', 'kea6']:
         #TODO: import only one function!
-        clean_config = importlib.import_module("serversupport.%s.functions"  % (SERVER_TYPE))
-        clean_config.run_bindctl (True, 'clean')  
+        clean_config = importlib.import_module("serversupport.%s.functions" % (SERVER_TYPE))
+        clean_config.run_bindctl(True, 'clean')
         kill_bind10()
-#         try:
-#             if REL4_ADDR and (SERVER_TYPE  == 'kea4'):
-#                 with settings(host_string = MGMT_ADDRESS, user = MGMT_USERNAME, password = MGMT_PASSWORD):
-#                     run("route del -host %s" % (GIADDR4))
-#         except NameError:
-#             pass # most likely REL4_ADDR caused this exception -> we do not use relay
-    elif SERVER_TYPE in ['isc_dhcp6','dibbler']:
-        stop = importlib.import_module("serversupport.%s.functions"  % (SERVER_TYPE))
+    #         try:
+    #             if REL4_ADDR and (SERVER_TYPE  == 'kea4'):
+    #                 with settings(host_string = MGMT_ADDRESS, user = MGMT_USERNAME, password = MGMT_PASSWORD):
+    #                     run("route del -host %s" % (GIADDR4))
+    #         except NameError:
+    #             pass # most likely REL4_ADDR caused this exception -> we do not use relay
+    elif SERVER_TYPE in ['isc_dhcp6', 'dibbler']:
+        stop = importlib.import_module("serversupport.%s.functions" % (SERVER_TYPE))
         stop.stop_srv()
 
     if AUTO_ARCHIVE:
         archive_name = PROTO + '_' + SERVER_TYPE + '_' + time.strftime("%Y-%m-%d-%H:%M")
-        archive_name = archive_file_name (1, 'tests_results_archive/' + archive_name)
+        archive_name = archive_file_name(1, 'tests_results_archive/' + archive_name)
         make_tarfile(archive_name + '.tar.gz', 'tests_results')
 
     get_common_logger().info("Goodbye.")
-    
