84215b2f7beca57e991d3a5ad68b05cd5fc1e0c3
wlodek <wlodek@isc.org>
2014-04-30 11:11:53 +0200

    adding new configure values: SLEEP_TIME_1 and SLEEP_TIME_2

--- lettuce/features/serversupport/bind10.py
+++ lettuce/features/serversupport/bind10.py
@@ -20,7 +20,7 @@
 # 
 #
 
-from features.init_all import SERVER_INSTALL_DIR
+from features.init_all import SERVER_INSTALL_DIR, SLEEP_TIME_1
 from features.logging_facility import get_common_logger
 
 from multi_server_functions import fabric_sudo_command 
@@ -31,7 +31,7 @@ def kill_bind10():
     Kill any running bind10 instance
     """
     get_common_logger().debug("Killing all running Bind instances")
-    return fabric_sudo_command('pkill b10-*; sleep 2')
+    return fabric_sudo_command('pkill b10-*; sleep ' + str(SLEEP_TIME_1))
 
 
 def start_bind10():
@@ -40,4 +40,5 @@ def start_bind10():
     """
     get_common_logger().debug("Starting Bind instances")
 
-    return fabric_sudo_command('(rm nohup.out; nohup ' + SERVER_INSTALL_DIR + 'sbin/bind10 &); sleep 2')
+    return fabric_sudo_command('(rm nohup.out; nohup ' + SERVER_INSTALL_DIR +
+                               'sbin/bind10 &); sleep ' + str(SLEEP_TIME_1))
