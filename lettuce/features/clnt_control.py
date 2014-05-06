5e7a36a1e862cb2a17bae2eba4b3d112ec32dc7b
Maciej <macfij7@wp.pl>
2014-05-06 11:20:22 +0200

    new steps for controlling dhcpv6 client

--- lettuce/features/clnt_control.py
+++ lettuce/features/clnt_control.py
@@ -2,9 +2,17 @@ from lettuce import world, step
 from init_all import SOFTWARE_UNDER_TEST
 import importlib
 
-dhcpfun = importlib.import_module("softwaresupport.%s.functions"  % SOFTWARE_UNDER_TEST)
+clntFunc = importlib.import_module("softwaresupport.%s.functions"  % SOFTWARE_UNDER_TEST)
 
+
+@step("Setting up test.")
+def client_setup(step):
+    clntFunc.client_setup(step)
 
 @step('Client is started.')
 def config_srv_subnet(step):
-   dhcpfun.start_clnt()
+   clntFunc.start_clnt(step)
+
+@step("Client is configured to request (\S+) option.")
+def client_option_req(step, opt):
+    clntFunc.client_option_req(step, opt)
--- lettuce/features/softwaresupport/dibbler_client/functions.py
+++ lettuce/features/softwaresupport/dibbler_client/functions.py
@@ -1,44 +1,68 @@
 from features.softwaresupport.multi_server_functions import fabric_sudo_command, \
-   fabric_send_file, fabric_run_command, fabric_remove_file_command
+    fabric_send_file, fabric_run_command, fabric_remove_file_command
 from logging_facility import *
 from lettuce.registry import world
 from init_all import DIBBLER_INSTALL_DIR, IFACE
 
 
-
 def restart_clnt():
-   fabric_sudo_command("("+DIBBLER_INSTALL_DIR+"dibbler-client stop); sleep 1;")
-   fabric_sudo_command("("+DIBBLER_INSTALL_DIR+"dibbler-client start); sleep 1;")
+    fabric_sudo_command("("+DIBBLER_INSTALL_DIR+"dibbler-client stop); sleep 1;")
+    fabric_sudo_command("("+DIBBLER_INSTALL_DIR+"dibbler-client start); sleep 1;")
 
 
 def stop_clnt():
-   fabric_sudo_command ("("+DIBBLER_INSTALL_DIR+"dibbler-client stop); sleep 1;")
+    fabric_sudo_command ("("+DIBBLER_INSTALL_DIR+"dibbler-client stop); sleep 1;")
 
 
-def prepare_cfg_default_clnt():
-   world.clntCfg = {}
-   world.clntCfg["Filename"] = "temp"
-   cfgFile = open(world.clntCfg["Filename"], "w")
-   openBracket = "{"
-   closeBracket = "}"
-   eth = IFACE
-   config = """log-level 8
+def create_clnt_cfg():
+    # generate a default config for client
+    openBracket = "{"
+    closeBracket = "}"
+    eth = IFACE
+    world.clntCfg["config"] = """log-level 8
 log-mode syslog
 duid-type duid-llt
 iface {eth} {openBracket}
-    pd {openBracket}
+{closeBracket}""".format(**locals())
+
+
+def client_option_req(step, opt):
+    # add option that client requests to default interface
+    openBracket = "{"
+    closeBracket = "}"
+    assert opt is not None, "No option given."
+
+    # delete closing bracket from iface scope,
+    # then add proper option and close the iface scope
+    input_idx = len(world.clntCfg["config"])-1
+    world.clntCfg["config"] = world.clntCfg["config"][:input_idx]
+    if opt == "IA_PD":
+        world.clntCfg["config"] += """    pd {openBracket}
+        prefix {openBracket}
+            preferred-lifetime 1000
+            valid-lifetime 2000
+        {closeBracket}
     {closeBracket}
-{closeBracket}
-   """.format(**locals())
-   cfgFile.write(config)
-   cfgFile.close()
+{closeBracket}""".format(**locals())
 
 
-def start_clnt():
-   # stop_clnt()
-   prepare_cfg_default_clnt()
-   get_common_logger().debug("Starting Dibbler Client with generated config:")
-   fabric_send_file(world.clntCfg["Filename"], '/etc/dibbler/client.conf')
-   fabric_remove_file_command(world.clntCfg["Filename"])
-   fabric_run_command ('(rm nohup.out; nohup '+DIBBLER_INSTALL_DIR+'dibbler-client start & ); sleep 2;')
-   # stop_clnt()
+def write_clnt_cfg_to_file():
+    # write generated config to a file
+    world.clntCfg["Filename"] = "temp"
+    cfgFile = open(world.clntCfg["Filename"], "w")
+    cfgFile.write(world.clntCfg["config"])
+    cfgFile.close()
+
+
+def client_setup(step):
+    # step for initializing client config
+    create_clnt_cfg()
+
+
+def start_clnt(step):
+    # step for writing config to file, send it and start client
+    write_clnt_cfg_to_file()
+    get_common_logger().debug("Starting Dibbler Client with generated config:")
+    fabric_send_file(world.clntCfg["Filename"], '/etc/dibbler/client.conf')
+    fabric_remove_file_command(world.clntCfg["Filename"])
+    fabric_run_command ('(rm nohup.out; nohup '+DIBBLER_INSTALL_DIR+'dibbler-client start & ); sleep 2;')
