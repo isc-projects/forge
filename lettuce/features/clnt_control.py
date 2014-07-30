07be86f76c13ba8ae09df99d448ffa00af1527e1
Wlodek Wencel <wlodek@isc.org>
2014-07-30 14:05:42 +0200

    new SOFTWARE_UNDER_TEST format, no backwards compatibility

--- lettuce/features/clnt_control.py
+++ lettuce/features/clnt_control.py
@@ -1,18 +1,26 @@
 from lettuce import world, step
-from init_all import SOFTWARE_UNDER_TEST
+from init_all import SOFTWARE_UNDER_TEST, DHCP
 import importlib
 
-clntFunc = importlib.import_module("softwaresupport.%s.functions"  % SOFTWARE_UNDER_TEST)
-
+DNS = ""
+for each_client_name in SOFTWARE_UNDER_TEST:
+    if each_client_name in DHCP:
+        dhcp = importlib.import_module("softwaresupport.%s.functions" % each_client_name)
+        world.cfg["dhcp_under_test"] = each_client_name
+    elif each_client_name in DNS:
+        # for future use, import DNS not declare it.
+        pass
 
 @step("Setting up test.")
 def client_setup(step):
-    clntFunc.client_setup(step)
+    dhcp.client_setup(step)
+
 
 @step('Client is started.')
 def config_srv_subnet(step):
-   clntFunc.start_clnt(step)
+    dhcp.start_clnt(step)
+
 
 @step("Client is configured to request (\S+) option.")
 def client_option_req(step, opt):
-    clntFunc.client_option_req(step, opt)
+    dhcp.client_option_req(step, opt)
