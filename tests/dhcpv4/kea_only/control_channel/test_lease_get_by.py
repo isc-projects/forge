c54ae6cc95893c3d07949d95514d17e0fb0bd987
Wlodek Wencel <wlodek@isc.org>
2020-08-20 13:58:10 +0200

    new leases-get tests

--- tests/dhcpv4/kea_only/control_channel/test_lease_get_by.py
+++ tests/dhcpv4/kea_only/control_channel/test_lease_get_by.py
@@ -58,6 +58,8 @@ def test_control_channel_lease4_get_by():
     srv_control.config_srv_another_subnet_no_interface('192.168.51.0/24',
                                                        '192.168.51.10-192.168.51.11')
 
+    world.dhcp_cfg.update({"ddns-send-updates": False})
+    world.dhcp_cfg["subnet4"][1].update({"ddns-send-updates": False})
     world.dhcp_cfg["subnet4"][0].update({"ddns-send-updates": True,
                                          "ddns-generated-prefix": "four",
                                          "ddns-qualifying-suffix": "example.com"})
@@ -70,7 +72,7 @@ def test_control_channel_lease4_get_by():
     srv_control.add_hooks('libdhcp_lease_cmds.so')
     srv_control.open_control_channel()
 
-    srv_control.add_ddns_server('127.0.0.1', 53000)
+    srv_control.add_ddns_server('127.0.0.1', 53001)
     srv_control.add_ddns_server_options('enable-updates', True)
     srv_control.add_forward_ddns('four.example.com.', 'EMPTY_KEY')
     srv_control.add_reverse_ddns('50.168.192.in-addr.arpa.', 'EMPTY_KEY')
@@ -78,22 +80,113 @@ def test_control_channel_lease4_get_by():
     srv_control.build_and_send_config_files('SSH', 'config-file')
     srv_control.start_srv('DHCP', 'started')
 
-    srv_control.use_dns_set_number(20)
-    srv_control.start_srv('DNS', 'started')
-
-    _send_cmd("config-get")
-
     _get_address("08:08:08:08:08:08", "192.168.50.5", cli_id="00010203040506", fqdn="four.hostname.com.")
     _get_address("09:09:09:09:09:09", "192.168.50.6", cli_id="00010203040507")
     _get_address("10:10:10:10:10:10", "192.168.51.10")
     _get_address("11:11:11:11:11:11", "192.168.51.11", fqdn="xyz.com.")
 
-    # let's get 08 for in a different ways
+    # let's get 08:08:08:08:08:08 for in a different ways
     by_id_1 = _send_cmd("lease4-get-by-client-id", extra_param={"client-id": "00010203040506"})
-    by_host_1 = _send_cmd("lease4-get-by-hostname", extra_param={"hostname": "four.hostname.com."})
-    by_hw_1 = _send_cmd("lease4-get-by-hw-address", extra_param={"hw-address": "08:08:08:08:08:08"})
-    by_hw_1 = _send_cmd("lease4-get-by-hw-address", extra_param={"hw-address": "10:10:10:10:10:10"})
-    by_hw_1 = _send_cmd("lease4-get-by-hw-address", extra_param={"hw-address": "11:11:11:11:11:11"})
+    del by_id_1["arguments"]["leases"][0]["cltt"]
+    assert by_id_1["arguments"]["leases"][0] == {"fqdn-fwd": True,
+                                                 "fqdn-rev": True,
+                                                 "client-id": "00:01:02:03:04:05:06",
+                                                 "hostname": "four.hostname.com.",
+                                                 "hw-address": "08:08:08:08:08:08",
+                                                 "ip-address": "192.168.50.5",
+                                                 "state": 0,
+                                                 "subnet-id": 1,
+                                                 "valid-lft": 4000}
 
-    by_host_2 = _send_cmd("lease4-get-by-hostname", extra_param={"hostname": "xyz.com."})
-    by_hw_2 = _send_cmd("lease4-get-by-hw-address", extra_param={"hw-address": "09:09:09:09:09:09"})
+    by_host_1 = _send_cmd("lease4-get-by-hostname", extra_param={"hostname": "four.hostname.com."})
+    del by_host_1["arguments"]["leases"][0]["cltt"]
+    assert by_host_1["arguments"]["leases"][0] == {"fqdn-fwd": True,
+                                                   "fqdn-rev": True,
+                                                   "client-id": "00:01:02:03:04:05:06",
+                                                   "hostname": "four.hostname.com.",
+                                                   "hw-address": "08:08:08:08:08:08",
+                                                   "ip-address": "192.168.50.5",
+                                                   "state": 0,
+                                                   "subnet-id": 1,
+                                                   "valid-lft": 4000}
+    by_hw_1 = _send_cmd("lease4-get-by-hw-address", extra_param={"hw-address": "08:08:08:08:08:08"})
+    del by_hw_1["arguments"]["leases"][0]["cltt"]
+    assert by_hw_1["arguments"]["leases"][0] == {"fqdn-fwd": True,
+                                                 "fqdn-rev": True,
+                                                 "client-id": "00:01:02:03:04:05:06",
+                                                 "hostname": "four.hostname.com.",
+                                                 "hw-address": "08:08:08:08:08:08",
+                                                 "ip-address": "192.168.50.5",
+                                                 "state": 0,
+                                                 "subnet-id": 1,
+                                                 "valid-lft": 4000}
+
+    # let's get 09:09:09:09:09:09 for in a different ways
+    by_id_1 = _send_cmd("lease4-get-by-client-id", extra_param={"client-id": "00010203040507"})
+    del by_id_1["arguments"]["leases"][0]["cltt"]
+    assert by_id_1["arguments"]["leases"][0] == {"fqdn-fwd": False,
+                                                 "fqdn-rev": False,
+                                                 "client-id": "00:01:02:03:04:05:07",
+                                                 "hw-address": "09:09:09:09:09:09",
+                                                 "ip-address": "192.168.50.6",
+                                                 "hostname": "",
+                                                 "state": 0,
+                                                 "subnet-id": 1,
+                                                 "valid-lft": 4000}
+
+    by_hw_1 = _send_cmd("lease4-get-by-hw-address", extra_param={"hw-address": "09:09:09:09:09:09"})
+    del by_hw_1["arguments"]["leases"][0]["cltt"]
+    assert by_hw_1["arguments"]["leases"][0] == {"fqdn-fwd": False,
+                                                 "fqdn-rev": False,
+                                                 "client-id": "00:01:02:03:04:05:07",
+                                                 "hw-address": "09:09:09:09:09:09",
+                                                 "ip-address": "192.168.50.6",
+                                                 "hostname": "",
+                                                 "state": 0,
+                                                 "subnet-id": 1,
+                                                 "valid-lft": 4000}
+
+    # let's get 11:11:11:11:11:11 for in a different ways
+    # THOSE TWO ARE FAILING
+    by_host_1 = _send_cmd("lease4-get-by-hostname", extra_param={"hostname": "xyz.com."})
+    del by_host_1["arguments"]["leases"][0]["cltt"]
+    assert by_host_1["arguments"]["leases"][0] == {"fqdn-fwd": False,
+                                                   "fqdn-rev": False,
+                                                   # "client-id": "", #TODO I think it should be added
+                                                   "hostname": "xyz.com.",
+                                                   "hw-address": "11:11:11:11:11:11",
+                                                   "ip-address": "192.168.51.11",
+                                                   "state": 0,
+                                                   "subnet-id": 2,
+                                                   "valid-lft": 4000}
+    by_hw_1 = _send_cmd("lease4-get-by-hw-address", extra_param={"hw-address": "11:11:11:11:11:11"})
+    del by_hw_1["arguments"]["leases"][0]["cltt"]
+    assert by_hw_1["arguments"]["leases"][0] == {"fqdn-fwd": False,
+                                                 "fqdn-rev": False,
+                                                 # "client-id": "", #TODO I think it should be added
+                                                 "hostname": "xyz.com.",
+                                                 "hw-address": "11:11:11:11:11:11",
+                                                 "ip-address": "192.168.51.11",
+                                                 "state": 0,
+                                                 "subnet-id": 2,
+                                                 "valid-lft": 4000}
+
+    by_hw_1 = _send_cmd("lease4-get-by-hw-address", extra_param={"hw-address": "10:10:10:10:10:10"})
+    del by_hw_1["arguments"]["leases"][0]["cltt"]
+    assert by_hw_1["arguments"]["leases"][0] == {"fqdn-fwd": False,
+                                                 "fqdn-rev": False,
+                                                 # "client-id": "", #TODO I think it should be added
+                                                 "hw-address": "10:10:10:10:10:10",
+                                                 "ip-address": "192.168.51.10",
+                                                 "hostname": "",
+                                                 "state": 0,
+                                                 "subnet-id": 2,
+                                                 "valid-lft": 4000}
+
+    _send_cmd("lease4-get-by-hw-address", extra_param={"hw-address": "11:11:12:12:13:13"}, exp_result=3)
+    _send_cmd("lease4-get-by-hostname", extra_param={"hostname": "abc.com."}, exp_result=3)
+    _send_cmd("lease4-get-by-client-id", extra_param={"client-id": "111111111111"}, exp_result=3)
+
+    _send_cmd("lease4-get-by-hw-address", exp_result=1)
+    _send_cmd("lease4-get-by-hostname", exp_result=1)
+    _send_cmd("lease4-get-by-client-id", exp_result=1)
