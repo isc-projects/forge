5954c16e504f517b48bdb8991a7f8379c313715e
Wlodek Wencel <wlodek@isc.org>
2020-11-30 12:08:29 +0100

    enable check-style.sh for HA tests

--- check-style.sh
+++ check-style.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e -x
 
-PY_FILES=`find tests/{dhcpv4,dhcpv6,other_tests}/ -name '*.py'`
+PY_FILES=`find tests/{dhcpv4,dhcpv6,other_tests,HA}/ -name '*.py'`
 PY_FILES="$PY_FILES feat2py.py"
 time pylint -j $(expr \( `nproc` + 1 \) / 2) --rcfile=pylint.rc $PY_FILES
 pycodestyle --max-line-length=3000 --ignore=E722 $PY_FILES
