d9043ba3132ff91fd8741aa5f92943dca9f2c448
Michal Nowikowski <godfryd@isc.org>
2020-12-15 12:45:03 +0100

    decline tests improvements
    - improved checks if DECLINE was really dropped by server
    - removed bad tests

--- tests/dhcpv4/process/test_v4_decline.py
+++ tests/dhcpv4/process/test_v4_decline.py
@@ -123,6 +123,9 @@ def test_v4_decline_success_short_decline_period():
 @pytest.mark.v4
 @pytest.mark.decline
 def test_v4_decline_fail_without_serverid():
+    # Send DECLINE for a lease but do not include server id.
+    # Such packet should be dropped by the server.
+    # See: https://www.ietf.org/rfc/rfc2131.txt, page 37
 
     misc.test_setup()
     srv_control.config_srv_subnet('192.168.50.0/24', '192.168.50.1-192.168.50.1')
@@ -149,6 +152,7 @@ def test_v4_decline_fail_without_serverid():
     srv_msg.send_wait_for_message('MUST', 'ACK')
     srv_msg.response_check_content('yiaddr', '192.168.50.1')
 
+    # send DECLINE without server id - it should be dropped
     misc.test_procedure()
     srv_msg.client_sets_value('Client', 'chaddr', '00:00:00:00:00:11')
     srv_msg.client_does_include_with_value('client_id', '00010203040111')
@@ -159,67 +163,15 @@ def test_v4_decline_fail_without_serverid():
     misc.pass_criteria()
     srv_msg.send_dont_wait_for_message()
 
-    misc.test_procedure()
-    srv_msg.client_sets_value('Client', 'chaddr', '00:00:00:00:00:22')
-    srv_msg.client_does_include_with_value('client_id', '00010203040122')
-    srv_msg.client_send_msg('DISCOVER')
-
-    misc.pass_criteria()
-    srv_msg.send_dont_wait_for_message()
-
-    misc.test_procedure()
-    srv_msg.client_sets_value('Client', 'chaddr', '00:00:00:00:00:11')
-    srv_msg.client_does_include_with_value('client_id', '00010203040111')
-    srv_msg.client_send_msg('DISCOVER')
-
-    misc.pass_criteria()
-    srv_msg.send_wait_for_message('MUST', 'OFFER')
-    srv_msg.response_check_content('yiaddr', '192.168.50.1')
-
-    # @v4 @dhcp4 @decline
-    # Scenario: v4.decline.fail-without-serverid
-    #
-    # Test Setup:
-    # Server is configured with 192.168.50.0/24 subnet with 192.168.50.1-192.168.50.1 pool.
-    # Send server configuration using SSH and config-file.
-    # DHCP server is started.
-    #
-    # Test Procedure:
-    # Client sends DISCOVER message.
-    #
-    # Pass Criteria:
-    # Server MUST respond with OFFER message.
-    # Response MUST contain yiaddr 192.168.50.1.
-    #
-    # Test Procedure:
-    # Client copies server_id option from received message.
-    # Client adds to the message requested_addr with value 192.168.50.1.
-    # Client sends REQUEST message.
-    #
-    # Pass Criteria:
-    # Server MUST respond with ACK message.
-    # Response MUST contain yiaddr 192.168.50.1.
-    #
-    # Test Procedure:
-    # Client sets ciaddr value to 0.0.0.0.
-    # Client adds to the message requested_addr with value 192.168.50.1.
-    # Client sends DECLINE message.
-    #
-    # Pass Criteria:
-    # Server MUST NOT respond.
-    #
-    # Test Procedure:
-    # Client sets chaddr value to 00:00:00:00:00:11.
-    # Client adds to the message client_id with value 00010203040111.
-    # Client sends DISCOVER message.
-    #
-    # Pass Criteria:
-    # Server MUST respond with NAK message.
+    # if it is dropped by the server then this log should not appear
+    # bug: # 1615
+    srv_msg.log_doesnt_contain(r'The lease will be unavailable for')
 
 
 @pytest.mark.v4
 @pytest.mark.decline
 def test_v4_decline_fail_without_requested_ip_address():
+    # See: https://www.ietf.org/rfc/rfc2131.txt, page 37
 
     misc.test_setup()
     srv_control.config_srv_subnet('192.168.50.0/24', '192.168.50.1-192.168.50.1')
@@ -246,6 +198,7 @@ def test_v4_decline_fail_without_requested_ip_address():
     srv_msg.send_wait_for_message('MUST', 'ACK')
     srv_msg.response_check_content('yiaddr', '192.168.50.1')
 
+    # send DECLINE without requested IP address - it should be dropped
     misc.test_procedure()
     srv_msg.client_sets_value('Client', 'chaddr', '00:00:00:00:00:11')
     srv_msg.client_does_include_with_value('client_id', '00010203040111')
@@ -256,85 +209,10 @@ def test_v4_decline_fail_without_requested_ip_address():
     misc.pass_criteria()
     srv_msg.send_dont_wait_for_message()
 
-    misc.test_procedure()
-    srv_msg.client_sets_value('Client', 'chaddr', '00:00:00:00:00:22')
-    srv_msg.client_does_include_with_value('client_id', '00010203040122')
-    srv_msg.client_send_msg('DISCOVER')
-
-    misc.pass_criteria()
-    srv_msg.send_dont_wait_for_message()
-
-    misc.test_procedure()
-    srv_msg.client_sets_value('Client', 'chaddr', '00:00:00:00:00:11')
-    srv_msg.client_does_include_with_value('client_id', '00010203040111')
-    srv_msg.client_send_msg('DISCOVER')
-
-    misc.pass_criteria()
-    srv_msg.send_wait_for_message('MUST', 'OFFER')
-    srv_msg.response_check_content('yiaddr', '192.168.50.1')
-    # client should get back this address because it's not in declined period
-
-
-@pytest.mark.v4
-@pytest.mark.decline
-def test_v4_decline_fail_client_id_not_included():
-
-    misc.test_setup()
-    srv_control.config_srv_subnet('192.168.50.0/24', '192.168.50.1-192.168.50.1')
-    srv_control.build_and_send_config_files('SSH', 'config-file')
-    srv_control.start_srv('DHCP', 'started')
-
-    misc.test_procedure()
-    srv_msg.client_sets_value('Client', 'chaddr', '00:00:00:00:00:22')
-    srv_msg.client_does_include_with_value('client_id', '00010203040122')
-    srv_msg.client_send_msg('DISCOVER')
-
-    misc.pass_criteria()
-    srv_msg.send_wait_for_message('MUST', 'OFFER')
-    srv_msg.response_check_content('yiaddr', '192.168.50.1')
-    srv_msg.response_check_include_option(61)
-    srv_msg.response_check_option_content(61, 'value', '00010203040122')
-
-    misc.test_procedure()
-    srv_msg.client_copy_option('server_id')
-    srv_msg.client_does_include_with_value('requested_addr', '192.168.50.1')
-    srv_msg.client_sets_value('Client', 'chaddr', '00:00:00:00:00:22')
-    srv_msg.client_does_include_with_value('client_id', '00010203040122')
-    srv_msg.client_send_msg('REQUEST')
-
-    misc.pass_criteria()
-    srv_msg.send_wait_for_message('MUST', 'ACK')
-    srv_msg.response_check_content('yiaddr', '192.168.50.1')
-    srv_msg.response_check_include_option(61)
-    srv_msg.response_check_option_content(61, 'value', '00010203040122')
-
-    misc.test_procedure()
-    srv_msg.client_sets_value('Client', 'ciaddr', '0.0.0.0')
-    srv_msg.client_copy_option('server_id')
-    srv_msg.client_does_include_with_value('requested_addr', '192.168.50.1')
-    srv_msg.client_send_msg('DECLINE')
-
-    misc.pass_criteria()
-    srv_msg.send_dont_wait_for_message()
-
-    misc.test_procedure()
-    srv_msg.client_sets_value('Client', 'chaddr', '00:00:00:00:00:11')
-    srv_msg.client_does_include_with_value('client_id', '00010203040999')
-    srv_msg.client_send_msg('DISCOVER')
-
-    misc.pass_criteria()
-    srv_msg.send_dont_wait_for_message()
-
-    misc.test_procedure()
-    srv_msg.client_sets_value('Client', 'chaddr', '00:00:00:00:00:22')
-    srv_msg.client_does_include_with_value('client_id', '00010203040122')
-    srv_msg.client_send_msg('DISCOVER')
-
-    misc.pass_criteria()
-    srv_msg.send_wait_for_message('MUST', 'OFFER')
-    srv_msg.response_check_content('yiaddr', '192.168.50.1')
-    srv_msg.response_check_include_option(61)
-    srv_msg.response_check_option_content(61, 'value', '00010203040122')
+    # if it is dropped by the server then this log should not appear
+    srv_msg.log_doesnt_contain(r'The lease will be unavailable for')
+    # and this one should appear
+    srv_msg.log_contains(r"failed to process packet: Mandatory 'Requested IP address' option missing in DHCPDECLINE")
 
 
 @pytest.mark.v4
@@ -368,7 +246,9 @@ def test_v4_decline_fail_different_client_id():
     srv_msg.response_check_include_option(61)
     srv_msg.response_check_option_content(61, 'value', '00010203040111')
 
+    # set different client ID (00010203040666) than it was sent in previous packets (00010203040111)
     misc.test_procedure()
+    srv_msg.client_does_include_with_value('requested_addr', '192.168.50.1')
     srv_msg.client_does_include_with_value('client_id', '00010203040666')
     srv_msg.client_sets_value('Client', 'ciaddr', '0.0.0.0')
     srv_msg.client_copy_option('server_id')
@@ -377,13 +257,8 @@ def test_v4_decline_fail_different_client_id():
     misc.pass_criteria()
     srv_msg.send_dont_wait_for_message()
 
-    misc.test_procedure()
-    srv_msg.client_sets_value('Client', 'chaddr', '00:00:00:00:00:11')
-    srv_msg.client_does_include_with_value('client_id', '00010203040999')
-    srv_msg.client_send_msg('DISCOVER')
-
-    misc.pass_criteria()
-    srv_msg.send_wait_for_message('MUST', 'NAK')
+    # the server should say that client IDs do not match
+    srv_msg.log_contains(r"Received DHCPDECLINE for addr 192.168.50.1.*but the data doesn't match:")
 
 
 @pytest.mark.v4
@@ -396,6 +271,7 @@ def test_v4_decline_fail_different_chaddr():
     srv_control.start_srv('DHCP', 'started')
 
     misc.test_procedure()
+    srv_msg.client_sets_value('Client', 'chaddr', '00:00:00:00:00:11')
     srv_msg.client_send_msg('DISCOVER')
 
     misc.pass_criteria()
@@ -403,6 +279,7 @@ def test_v4_decline_fail_different_chaddr():
     srv_msg.response_check_content('yiaddr', '192.168.50.1')
 
     misc.test_procedure()
+    srv_msg.client_sets_value('Client', 'chaddr', '00:00:00:00:00:11')
     srv_msg.client_copy_option('server_id')
     srv_msg.client_does_include_with_value('requested_addr', '192.168.50.1')
     srv_msg.client_send_msg('REQUEST')
@@ -411,9 +288,11 @@ def test_v4_decline_fail_different_chaddr():
     srv_msg.send_wait_for_message('MUST', 'ACK')
     srv_msg.response_check_content('yiaddr', '192.168.50.1')
 
+    # set different client hw addr (00:00:00:00:00:22)
+    # than it was sent in previous packets (00:00:00:00:00:11)
     misc.test_procedure()
     srv_msg.client_copy_option('server_id')
-    srv_msg.client_sets_value('Client', 'chaddr', '00:00:00:00:00:11')
+    srv_msg.client_sets_value('Client', 'chaddr', '00:00:00:00:00:22')
     srv_msg.client_sets_value('Client', 'ciaddr', '0.0.0.0')
     srv_msg.client_does_include_with_value('requested_addr', '192.168.50.1')
     srv_msg.client_send_msg('DECLINE')
@@ -421,10 +300,5 @@ def test_v4_decline_fail_different_chaddr():
     misc.pass_criteria()
     srv_msg.send_dont_wait_for_message()
 
-    misc.test_procedure()
-    srv_msg.client_sets_value('Client', 'chaddr', '00:00:00:00:00:11')
-    srv_msg.client_does_include_with_value('client_id', '00010203040111')
-    srv_msg.client_send_msg('DISCOVER')
-
-    misc.pass_criteria()
-    srv_msg.send_wait_for_message('MUST', 'NAK')
+    # the server should say that client IDs do not match
+    srv_msg.log_contains(r"Received DHCPDECLINE for addr 192.168.50.1.*but the data doesn't match: received hwaddr: 00:00:00:00:00:22, lease hwaddr: 00:00:00:00:00:11")
