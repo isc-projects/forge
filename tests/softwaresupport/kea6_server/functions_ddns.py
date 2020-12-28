342141035005ce87b856318d69e0404977acd895
Andrei Pavel <andrei@isc.org>
2020-12-28 15:28:23 +0200

    amend outdated copyrights

--- tests/softwaresupport/kea6_server/functions_ddns.py
+++ tests/softwaresupport/kea6_server/functions_ddns.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2013-2019 Internet Systems Consortium.
+# Copyright (C) 2013-2020 Internet Systems Consortium.
 #
 # Permission to use, copy, modify, and distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
