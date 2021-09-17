952fe4e00b83efc45645fc452807ddb1fdb52d4b
Andrei Pavel <andrei@isc.org>
2021-09-17 14:26:18 +0300

    less verbose check-style.sh

--- check-style.sh
+++ check-style.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-set -e -x
 
-PY_FILES=`find tests/{dhcpv4,dhcpv6,other_tests,HA}/ -name '*.py'`
-time pylint -j $(expr \( `nproc` + 1 \) / 2) --rcfile=pylint.rc --disable=C0209 $PY_FILES
-pycodestyle --max-line-length=3000 --ignore=E722 $PY_FILES
+set -eu
+
+script_path=$(cd "$(dirname "${0}")" && pwd)
+
+cd "${script_path}"
+
+PY_FILES="$(find tests/{dhcpv4,dhcpv6,other_tests,HA}/ -name '*.py' | sort -V)"
+printf 'Checking %s files...\n' "$(printf '%s\n' "${PY_FILES}" | wc -w)"
+
+pylint -j "$(nproc || gnproc || echo 1)" --rcfile=pylint.rc --disable=C0209 ${PY_FILES}
+pycodestyle --max-line-length=3000 ${PY_FILES}
