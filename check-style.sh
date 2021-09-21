a9e9b10808d782b5a1a264fdf412a4b1669aad03
Wlodek Wencel <wlodek@isc.org>
2021-09-21 12:07:30 +0200

    disabled pylint error C0209

--- check-style.sh
+++ check-style.sh
@@ -2,5 +2,5 @@
 set -e -x
 
 PY_FILES=`find tests/{dhcpv4,dhcpv6,other_tests,HA}/ -name '*.py'`
-time pylint -j $(expr \( `nproc` + 1 \) / 2) --rcfile=pylint.rc $PY_FILES
+time pylint -j $(expr \( `nproc` + 1 \) / 2) --rcfile=pylint.rc --disable=C0209 $PY_FILES
 pycodestyle --max-line-length=3000 --ignore=E722 $PY_FILES
