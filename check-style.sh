4ff80ce82c6e97852828219ba36029909eb2b969
Wlodek Wencel <wlodek@isc.org>
2021-12-01 13:51:50 +0100

    style fixes, check-style can be run with specific file

--- check-style.sh
+++ check-style.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-set -e -x
+set -eu
 
-PY_FILES=`find tests/{dhcpv4,dhcpv6,other_tests,HA}/ -name '*.py'`
-time pylint -j $(expr \( `nproc` + 1 \) / 2) --rcfile=pylint.rc --disable=C0209 $PY_FILES
-pycodestyle --max-line-length=3000 --ignore=E722 $PY_FILES
+script_path=$(cd "$(dirname "${0}")" && pwd)
+
+cd "${script_path}"
+if [ $# -eq 0 ]
+  then
+    PY_FILES="$(find tests/{dhcpv4,dhcpv6,other_tests,HA,iscdhcp}/ -name '*.py' | sort -V)"
+  else
+    PY_FILES="$@"
+fi
+
+printf 'Checking %s files...\n' "$(printf '%s\n' "${PY_FILES}" | wc -w)"
+
+pylint -j "$(nproc || gnproc || echo 1)" --rcfile=pylint.rc --disable=C0209 ${PY_FILES}
+pycodestyle --max-line-length=3000 ${PY_FILES}
