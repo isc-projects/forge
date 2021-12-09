ebed4a80adc10acae6280b5a185988d43f14e696
Andrei Pavel <andrei@isc.org>
2021-12-09 15:24:47 +0200

    add some basic bootp tests

--- tests/dhcpv4/kea_only/test_bootp.py
+++ tests/dhcpv4/kea_only/test_bootp.py
@@ -30,6 +30,50 @@ from forge_cfg import world
 def test_bootp_basic_request_reply(backend):
     misc.test_setup()
     srv_control.define_temporary_lease_db_backend(backend)
+    srv_control.config_srv_subnet('192.168.50.0/24', '192.168.50.1-192.168.50.10')
+    srv_control.add_hooks('libdhcp_bootp.so')
+    srv_control.build_and_send_config_files()
+    srv_control.start_srv('DHCP', 'started')
+
+    # DORA should ACK.
+    srv_msg.DORA('192.168.50.1', chaddr='ff:11:11:11:11:11')
+
+    # First exchange
+    srv_msg.BOOTP_REQUEST_and_BOOTP_REPLY('192.168.50.2', chaddr='ff:22:22:22:22:22')
+
+    # A second exchange should get the same lease.
+    srv_msg.BOOTP_REQUEST_and_BOOTP_REPLY('192.168.50.2', chaddr='ff:22:22:22:22:22')
+
+
+@pytest.mark.v4
+@pytest.mark.v4_bootp
+@pytest.mark.parametrize('backend', ['memfile', 'mysql', 'postgresql'])
+@pytest.mark.parametrize('bootp_first', [False, True])
+def test_bootp_basic_request_reply_same_chaddr(backend, bootp_first):
+    misc.test_setup()
+    srv_control.define_temporary_lease_db_backend(backend)
+    srv_control.config_srv_subnet('192.168.50.0/24', '192.168.50.1-192.168.50.10')
+    srv_control.add_hooks('libdhcp_bootp.so')
+    srv_control.build_and_send_config_files()
+    srv_control.start_srv('DHCP', 'started')
+
+    # Run twice to check that the same request gets the same lease the second
+    # time.
+    for i in range(2):
+        if bootp_first and i == 0:
+            # BOOTP with the same chaddr should get the same lease.
+            srv_msg.BOOTP_REQUEST_and_BOOTP_REPLY('192.168.50.1')
+
+        # DORA should ACK.
+        srv_msg.DORA('192.168.50.1')
+
+
+@pytest.mark.v4
+@pytest.mark.v4_bootp
+@pytest.mark.parametrize('backend', ['memfile', 'mysql', 'postgresql'])
+def test_bootp_basic_request_reply_classes(backend):
+    misc.test_setup()
+    srv_control.define_temporary_lease_db_backend(backend)
     srv_control.config_srv_subnet('192.168.50.0/24', '192.168.50.1-192.168.50.1')
     srv_control.new_pool('192.168.50.10-192.168.50.10', 0)
     srv_control.add_hooks('libdhcp_bootp.so')
@@ -42,10 +86,10 @@ def test_bootp_basic_request_reply(backend):
     srv_control.start_srv('DHCP', 'started')
 
     # First exchange
-    srv_msg.BOOTP_REQUEST_and_BOOTP_REPLY('192.168.50.1')
+    srv_msg.BOOTP_REQUEST_and_BOOTP_REPLY('192.168.50.1', chaddr='ff:11:11:11:11:11')
 
     # Let's test that we still get a lease on a second exchange.
-    srv_msg.BOOTP_REQUEST_and_BOOTP_REPLY('192.168.50.1')
+    srv_msg.BOOTP_REQUEST_and_BOOTP_REPLY('192.168.50.1', chaddr='ff:11:11:11:11:11')
 
     # DORA should still work.
-    srv_msg.DORA('192.168.50.10')
+    srv_msg.DORA('192.168.50.10', chaddr='ff:22:22:22:22:22')
