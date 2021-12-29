a876deae481f40da22e9676836076250b25d9171
Wlodek Wencel <wlodek@isc.org>
2021-12-29 22:01:30 +0100

    change fedora krb installation

--- tests/softwaresupport/krb.py
+++ tests/softwaresupport/krb.py
@@ -50,14 +50,14 @@ def install_krb(dns_addr, domain, key_life=2):
     fabric_sudo_command('kdestroy -A', ignore_errors=True)
     manage_kerb()
     if world.server_system == 'redhat':
-        fabric_sudo_command("dnf remove -y krb5-server krb5-workstation", ignore_errors=True)
-        fabric_sudo_command("dnf install -y krb5-server krb5-workstation")
+        pass
+        # fabric_sudo_command("dnf remove -y krb5-server krb5-workstation", ignore_errors=True)
+        # fabric_sudo_command("dnf install -y krb5-server krb5-workstation", ignore_errors=True)
     else:
         fabric_sudo_command('apt-get purge -y krb5-kdc krb5-admin-server libkrb5-dev dnsutils krb5-user', ignore_errors=True)
         fabric_sudo_command('rm -rf /var/lib/krb5kdc /etc/krb5kdc /etc/krb5kdc/kadm5.acl /var/tmp/DNS_0 /var/tmp/kadmin_0 /tmp/krb5cc_0 /tmp/krb5*')
-        fabric_sudo_command('rm -rf /tmp/dhcp.keytab /tmp/dns.keytab /tmp/krb5cc_0 /tmp/krb5*')
         fabric_sudo_command('sudo DEBIAN_FRONTEND=noninteractive apt install -y krb5-kdc krb5-admin-server libkrb5-dev dnsutils krb5-user')
-
+    fabric_sudo_command('rm -rf /tmp/dhcp.keytab /tmp/dns.keytab /tmp/krb5cc_0 /tmp/krb5*')
     # /etc/krb5.conf
     krb5_conf = f"""[libdefaults]
             default_realm = {domain.upper()}
@@ -168,6 +168,7 @@ def init_and_start_krb(dns_addr, domain, key_life=2):
                              user_loc='wlodek',
                              password_loc='DdU3Hjb2')
         # than we can upload this to system that is running kea
+        fabric_sudo_command(f"rm -rf /tmp/forge{domain[3:7]}.keytab")
         fabric_send_file(os.path.join(world.cfg["test_result_dir"], f"forge{domain[3:7]}.keytab"),
                          f"/tmp/forge{domain[3:7]}.keytab")
 
