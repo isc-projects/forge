af5da013fe589ba06b8a76989d3247fb14bff914
Wlodek Wencel <wlodek@isc.org>
2022-02-01 10:56:38 +0100

    example test using two vagrant systems

---
+++ tests/iscdhcp/v6/test_two_servers.py
@@ -0,0 +1,82 @@
+"""ISC_DHCP DHCPv6 two server setup test"""
+
+# pylint: disable=invalid-name,line-too-long
+
+import pytest
+import misc
+import srv_control
+import srv_msg
+
+from softwaresupport.isc_dhcp6_server.functions import add_line_in_global
+from forge_cfg import world
+
+
+@pytest.fixture(autouse=True)
+def kill_kea_on_second_system():
+    # kill kea and clear data at the beginning and at the end
+    srv_control.clear_some_data('all', dest=world.f_cfg.mgmt_address_2)
+    srv_control.start_srv('DHCP', 'stopped', dest=world.f_cfg.mgmt_address_2)
+    yield
+    srv_control.clear_some_data('all', dest=world.f_cfg.mgmt_address_2)
+    srv_control.clear_some_data('all', service='dns', dest=world.f_cfg.mgmt_address_2)
+    srv_control.start_srv('DHCP', 'stopped', dest=world.f_cfg.mgmt_address_2)
+    # TODO figure out why we don't download all at the end, in kea HA we do
+
+
+def _get_address(duid, address):
+    misc.test_procedure()
+    srv_msg.client_sets_value('Client', 'ia_id', 666)
+    srv_msg.client_sets_value('Client', 'DUID', duid)
+    srv_msg.client_does_include('Client', 'client-id')
+    srv_msg.client_does_include('Client', 'IA-NA')
+    srv_msg.client_send_msg('SOLICIT')
+
+    misc.pass_criteria()
+    srv_msg.send_wait_for_message('MUST', 'ADVERTISE')
+    srv_msg.response_check_include_option(1)
+    srv_msg.response_check_include_option(2)
+    srv_msg.response_check_include_option(3)
+    srv_msg.response_check_option_content(3, 'sub-option', 5)
+
+    misc.test_procedure()
+    srv_msg.client_sets_value('Client', 'ia_id', 666)
+    srv_msg.client_sets_value('Client', 'DUID', duid)
+    srv_msg.client_copy_option('server-id')
+    srv_msg.client_copy_option('IA_NA')
+    srv_msg.client_does_include('Client', 'client-id')
+    srv_msg.client_send_msg('REQUEST')
+
+    misc.pass_criteria()
+    srv_msg.send_wait_for_message('MUST', 'REPLY')
+    srv_msg.response_check_include_option(3)
+    srv_msg.response_check_option_content(3, 'sub-option', 5)
+    srv_msg.response_check_suboption_content(5, 3, 'addr', address)
+
+
+@pytest.mark.v6
+@pytest.mark.dhcpd
+def test_dhcpd_two_servers():
+    """
+    This is example test, how to use forge + vagrant setup to execute tests using both virtual systems
+    """
+    # start first server
+    misc.test_setup()
+    srv_control.config_srv_subnet('2001:db8:1::/64', '2001:db8:1::50-2001:db8:1::50')
+    srv_control.build_and_send_config_files()
+    srv_control.start_srv('DHCP', 'started')
+
+    # get an address
+    _get_address('00:03:00:01:f6:f5:f4:f3:f2:01', "2001:db8:1::50")
+
+    # stop server
+    srv_control.start_srv('DHCP', 'stopped')
+
+    # start second server, setp misc.test_setup() is mandatory!
+    misc.test_setup()
+    srv_control.config_srv_subnet('2001:db8:1::/64', '2001:db8:1::150-2001:db8:1::150')
+    srv_control.build_and_send_config_files(dest=world.f_cfg.mgmt_address_2)
+    srv_control.start_srv('DHCP', 'started', dest=world.f_cfg.mgmt_address_2)
+
+    # get an address
+    _get_address('00:03:00:01:f6:f5:f4:f3:f2:01', "2001:db8:1::150")
+    srv_control.start_srv('DHCP', 'stopped', dest=world.f_cfg.mgmt_address_2)
