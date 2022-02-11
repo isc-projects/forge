bda9a8fdce9c528f01f35bbe194c6e7dec739423
Andrei Pavel <andrei@isc.org>
2022-02-11 12:39:04 +0200

    enable SAST in Gitlab CI

---
+++ .gitlab/ci/Dockerfile
@@ -0,0 +1,4 @@
+FROM python:latest AS gitlab-ci
+
+# Install dependencies.
+RUN pip install bandit pycodestyle pylint pytest semgrep
---
+++ .gitlab/ci/build-image.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+docker login registry.gitlab.isc.org
+docker build --no-cache -t registry.gitlab.isc.org/isc-projects/forge ./
+docker push registry.gitlab.isc.org/isc-projects/forge
