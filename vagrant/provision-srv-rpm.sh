66c510eec3cf8b6d0e0b8169c6ac292edb759f5c
Andrei Pavel <andrei@isc.org>
2022-03-25 13:02:32 +0200

    generate certificates for FreeRADIUS on Fedora

--- vagrant/provision-srv-rpm.sh
+++ vagrant/provision-srv-rpm.sh
@@ -9,3 +9,6 @@ sudo ip -6 route add 2001:db8:1::/64 dev enp0s9
 # enable access to pgsql
 sudo sed -i -e s/ident/md5/ /var/lib/pgsql/data/pg_hba.conf
 sudo systemctl restart postgresql
+
+# Generate certificates for the FreeRADIUS server.
+sudo /etc/raddb/certs/bootstrap
