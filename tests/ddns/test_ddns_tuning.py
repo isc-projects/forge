7c2b45cde749fd7d079a65a72699d1f48ed9ad73
mgodzina <mgodzina@isc.org>
2022-04-12 17:21:20 +0100

    Added first ddn_tuning test

--- tests/ddns/test_ddns_tuning.py
+++ tests/ddns/test_ddns_tuning.py
@@ -17,22 +17,60 @@ from src import srv_msg
 from src import srv_control
 
 
+def _get_address(duid, fqdn):
+    misc.test_procedure()
+    srv_msg.client_sets_value('Client', 'DUID', duid)
+    srv_msg.client_does_include('Client', 'client-id')
+    srv_msg.client_does_include('Client', 'IA-NA')
+    srv_msg.client_send_msg('SOLICIT')
+
+    misc.pass_criteria()
+    srv_msg.send_wait_for_message('MUST', 'ADVERTISE')
+
+    misc.test_procedure()
+    srv_msg.client_sets_value('Client', 'DUID', duid)
+    srv_msg.client_copy_option('IA_NA')
+    srv_msg.client_copy_option('server-id')
+    if fqdn is not None:
+        srv_msg.client_sets_value('Client', 'FQDN_domain_name', fqdn)
+        srv_msg.client_sets_value('Client', 'FQDN_flags', 'S')
+        srv_msg.client_does_include('Client', 'fqdn')
+    srv_msg.client_does_include('Client', 'client-id')
+    srv_msg.client_send_msg('REQUEST')
+
+    misc.pass_criteria()
+    srv_msg.send_wait_for_message('MUST', 'REPLY')
+
+
 @pytest.mark.v4
+@pytest.mark.v6
 @pytest.mark.ddns
-def test_ddns_tuning_basic():
+@pytest.mark.parametrize('backend', ['memfile', 'mysql', 'postgresql'])
+def test_ddns_tuning_basic(backend, dhcp_version):
     misc.test_setup()
-    srv_control.config_srv_subnet('192.168.50.0/24', '192.168.50.1-192.168.50.10')
+    srv_control.define_temporary_lease_db_backend(backend)
+    if dhcp_version == 'v4':
+        srv_control.config_srv_subnet('192.168.50.0/24', '192.168.50.1-192.168.50.10')
+    else:
+        srv_control.config_srv_subnet('2001:db8:1::/64', '2001:db8:1::1-2001:db8:1::21')
     srv_control.add_hooks('libdhcp_ddns_tuning.so')
-    srv_control.add_parameter_to_hook(1, "hostname-expr", "'host-'+hexstring(pkt4.mac,'-')")
+    if dhcp_version == 'v4':
+        srv_control.add_parameter_to_hook(1, "hostname-expr", "'host-'+hexstring(pkt4.mac,'-')")
+    else:
+        srv_control.add_parameter_to_hook(1, "hostname-expr", "'host-'+hexstring(option[1].hex, '-')")
     srv_control.add_hooks('libdhcp_lease_cmds.so')
     srv_control.open_control_channel()
     srv_control.agent_control_channel()
     srv_control.build_and_send_config_files()
     srv_control.start_srv('DHCP', 'started')
 
-    srv_msg.DORA('192.168.50.1')
-    cmd = {"command": "lease4-get-all"}
-    response = srv_msg.send_ctrl_cmd(cmd, 'http')
-
-    assert response['arguments']['leases'][0]['hostname'] == 'host-ff-01-02-03-ff-04'
-
+    if dhcp_version == 'v4':
+        srv_msg.DORA('192.168.50.1')
+        cmd = {"command": "lease4-get-all"}
+        response = srv_msg.send_ctrl_cmd(cmd, 'http')
+        assert response['arguments']['leases'][0]['hostname'] == 'host-ff-01-02-03-ff-04'
+    else:
+        _get_address(duid='00:03:00:01:66:55:44:33:22:11', fqdn='test.com')
+        cmd = {"command": "lease6-get-all"}
+        response = srv_msg.send_ctrl_cmd(cmd, 'http')
+        assert response['arguments']['leases'][0]['hostname'] == 'host-00-03-00-01-66-55-44-33-22-11.'
