1e07b6483eada0775129bcb1d25d2d83d472fb55
Marcin Godzina <mgodzina@isc.org>
2022-05-13 08:39:19 +0000

    added comment

--- tests/ddns/test_ddns6.py
+++ tests/ddns/test_ddns6.py
@@ -1435,6 +1435,7 @@ def test_ddns6_notsig_rev_Nflag_override_no_update():
     srv_msg.response_check_include_option(39)
     srv_msg.response_check_option_content(39, 'flags', 'SO')
     srv_msg.response_check_option_content(39, 'fqdn', 'sth6.six.example.com.')
+    # Optional DNS tests
     # srv_msg.dns_log_contains(None,
     #                          'adding an RR at \'0.5.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.1.0.0.0.8.b.d.0.1.0.0.2.ip6.arpa\' PTR sth6.six.example.com.')
 
