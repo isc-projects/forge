09707a91b6d3547f821335c6e9219306a0d38f86
Marcin Godzina <mgodzina@isc.org>
2022-05-13 08:39:30 +0000

    added comment

--- tests/ddns/test_ddns6.py
+++ tests/ddns/test_ddns6.py
@@ -1276,6 +1276,7 @@ def test_ddns6_notsig_rev_success_withoutflag():
     srv_msg.response_check_option_content(39, 'flags', 'N', expect_include=False)
     srv_msg.response_check_option_content(39, 'flags', 'O', expect_include=False)
     srv_msg.response_check_option_content(39, 'fqdn', 'sth6.six.example.com.')
+    # Optional DNS tests
     # srv_msg.dns_log_contains(None,
     #                          'adding an RR at \'0.5.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.1.0.0.0.8.b.d.0.1.0.0.2.ip6.arpa\' PTR sth6.six.example.com.')
 
