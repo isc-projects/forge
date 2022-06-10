15dfb3fba1e02c44e634ad3bf61aa2fcc19f7073
Marcin Godzina <mgodzina@isc.org>
2022-06-10 12:19:37 +0200

    assertion added to subnet and class

--- tests/dhcp/hooks/test_limits.py
+++ tests/dhcp/hooks/test_limits.py
@@ -19,7 +19,7 @@ from src import srv_msg
 from src.forge_cfg import world
 
 
-def _get_address_v4(address, chaddr, vendor=None):
+def _get_address_v4(chaddr, vendor=None):
     misc.test_procedure()
     srv_msg.client_sets_value('Client', 'chaddr', chaddr)
     if vendor is not None:
@@ -36,7 +36,7 @@ def _get_address_v4(address, chaddr, vendor=None):
     return 1
 
 
-def _get_address_v6(address, duid, vendor=None):
+def _get_address_v6(duid, vendor=None):
     misc.test_procedure()
     srv_msg.client_sets_value('Client', 'DUID', duid)
     srv_msg.client_does_include('Client', 'client-id')
@@ -66,7 +66,7 @@ def test_limits_subnet(dhcp_version, backend):
     if dhcp_version == 'v4':
         srv_control.config_srv_subnet('192.168.0.0/16', '192.168.1.1-192.168.255.255')
         srv_control.config_srv_opt('subnet-mask', '255.255.0.0')
-        limit = 20
+        limit = 15
     else:
         srv_control.config_srv_subnet('2001:db8:1::/64', '2001:db8:1::1-2001:db8:1::255:255')
         limit = 3
@@ -90,12 +90,13 @@ def test_limits_subnet(dhcp_version, backend):
     else:
         world.cfg['wait_interval'] = 0.1
     start = time.time()
-    for k in range(1, 10):
-        for i in range(1, 30):
-            if dhcp_version == 'v4':
-                success += _get_address_v4(f'192.168.{k}.{i}', chaddr=f'ff:01:02:03:{k:0>2x}:{i:0>2x}')
-            else:
-                success += _get_address_v6(f'2001:db8:1::{k}:{i}', duid=f'00:03:00:01:ff:ff:ff:ff:{k:0>2x}:{i:0>2x}')
+    if dhcp_version == 'v4':
+        for i in range(1, 90):
+            success += _get_address_v4(chaddr=f'ff:01:02:03:04:05')
+            packets += 1
+    else:
+        for i in range(1, 20):
+            success += _get_address_v6(duid=f'00:03:00:01:ff:ff:ff:ff:ff:ff')
             packets += 1
     end = time.time()
     run1 = end - start
@@ -103,6 +104,9 @@ def test_limits_subnet(dhcp_version, backend):
     print(f"Runtime of the program is {run1}")
     print(f"Packets received {success}/{packets}")
     print(f"Packets per second {success / run1}")
+
+    score = success / run1
+    assert abs(limit-score) < 1
 
 
 @pytest.mark.v4
@@ -122,18 +126,20 @@ def test_limits_class(dhcp_version, backend):
     srv_control.add_hooks('libdhcp_limits.so')
     srv_control.add_hooks('libdhcp_class_cmds.so')
     if dhcp_version == 'v4':
+        limit = 15
         classes = [
             {
                 "name": "gold",
                 "test": "option[60].text == 'PXE'",
                 "user-context": {
                     "limits": {
-                        "rate-limit": "20 packets per second"
+                        "rate-limit": "15 packets per second"
                     }
                 }
             }
         ]
     else:
+        limit = 3
         classes = [
             {
                 "name": "VENDOR_CLASS_eRouter2.0",
@@ -157,12 +163,13 @@ def test_limits_class(dhcp_version, backend):
     else:
         world.cfg['wait_interval'] = 0.1
     start = time.time()
-    for k in range(1, 10):
-        for i in range(1, 30):
-            if dhcp_version == 'v4':
-                success += _get_address_v4(f'192.168.{k}.{i}', chaddr=f'ff:01:02:03:{k:0>2x}:{i:0>2x}', vendor='PXE')
-            else:
-                success += _get_address_v6(f'2001:db8:1::{k}:{i}', duid=f'00:03:00:01:ff:ff:ff:ff:{k:0>2x}:{i:0>2x}', vendor='eRouter1.0')
+    if dhcp_version == 'v4':
+        for i in range(1, 90):
+            success += _get_address_v4(chaddr=f'ff:01:02:03:04:05', vendor='PXE')
+            packets += 1
+    else:
+        for i in range(1, 20):
+            success += _get_address_v6(duid=f'00:03:00:01:ff:ff:ff:ff:ff:ff', vendor='eRouter2.0')
             packets += 1
     end = time.time()
     run1 = end - start
@@ -170,6 +177,9 @@ def test_limits_class(dhcp_version, backend):
     print(f"Runtime of the program is {run1}")
     print(f"Packets received {success}/{packets}")
     print(f"Packets per second {success / run1}")
+
+    score = success / run1
+    assert abs(limit-score) < 1
 
 
 @pytest.mark.v4
@@ -185,7 +195,7 @@ def test_limits_mix(dhcp_version, backend):
         limit = 20
     else:
         srv_control.config_srv_subnet('2001:db8:1::/64', '2001:db8:1::1-2001:db8:1::255:255')
-        limit = 10
+        limit = 30
     srv_control.add_line_to_subnet(0, {"user-context": {
         "limits": {
             "rate-limit": f"{limit} packets per second"
@@ -222,7 +232,7 @@ def test_limits_mix(dhcp_version, backend):
                 "name": "VENDOR_CLASS_eRouter1.0",
                 "user-context": {
                     "limits": {
-                        "rate-limit": "3 packets per second"
+                        "rate-limit": "10 packets per second"
                     }
                 }
             },
@@ -230,7 +240,7 @@ def test_limits_mix(dhcp_version, backend):
                 "name": "VENDOR_CLASS_eRouter2.0",
                 "user-context": {
                     "limits": {
-                        "rate-limit": "1 packets per second"
+                        "rate-limit": "10 packets per second"
                     }
                 }
             }
@@ -253,11 +263,11 @@ def test_limits_mix(dhcp_version, backend):
     for k in range(1, 10):
         for i in range(1, 30):
             if dhcp_version == 'v4':
-                success_gold += _get_address_v4(f'192.168.{k}.{i}', chaddr=f'ff:01:02:03:{k:0>2x}:{i:0>2x}', vendor='PXE')
-                success_silver += _get_address_v4(f'192.168.{k}.{i}', chaddr=f'ff:01:02:03:{k:0>2x}:{i:0>2x}', vendor='PXA')
+                success_gold += _get_address_v4(chaddr=f'ff:01:02:03:{k:0>2x}:{i:0>2x}', vendor='PXE')
+                success_silver += _get_address_v4(chaddr=f'ff:01:02:03:{k:0>2x}:{i:0>2x}', vendor='PXA')
             else:
-                success_gold += _get_address_v6(f'2001:db8:1::{k}:{i}', duid=f'00:03:00:01:ff:ff:ff:ff:{k:0>2x}:{i:0>2x}', vendor='eRouter1.0')
-                success_silver += _get_address_v6(f'2001:db8:1::{k}:{i}', duid=f'00:03:00:01:ff:ff:ff:ff:{k:0>2x}:{i:0>2x}', vendor='eRouter2.0')
+                success_gold += _get_address_v6(duid=f'00:03:00:01:ff:ff:ff:ff:{k:0>2x}:{i:0>2x}', vendor='eRouter1.0')
+                success_silver += _get_address_v6(duid=f'00:03:00:01:ff:ff:ff:ff:{k:0>2x}:{i:0>2x}', vendor='eRouter2.0')
             packets_gold += 1
             packets_silver += 1
     end = time.time()
