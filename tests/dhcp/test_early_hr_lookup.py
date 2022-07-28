1970dc504fc07f1fa9b7ef5472480b3c25fc5c7c
Marcin Godzina <mgodzina@isc.org>
2022-07-28 10:55:03 +0200

    Rearanged tests, added database support.

--- tests/dhcp/test_early_hr_lookup.py
+++ tests/dhcp/test_early_hr_lookup.py
@@ -6,7 +6,7 @@
 
 # Author: Marcin Godzina
 
-"""Host Reservation DHCPv4"""
+"""Early Global Host Reservation Lookup Tests"""
 
 # pylint: disable=invalid-name,line-too-long
 
@@ -20,6 +20,12 @@ from src.forge_cfg import world
 
 
 def _get_address_v4(yiaddr, chaddr, drop=False):
+    """
+    Helper function to get v4 lease or check for DROP
+    :param yiaddr: desired ip address
+    :param chaddr: MAC address
+    :param drop: If request should be dropped by Kea
+    """
     misc.test_procedure()
     srv_msg.client_sets_value('Client', 'chaddr', chaddr)
     srv_msg.client_send_msg('DISCOVER')
@@ -47,6 +53,12 @@ def _get_address_v4(yiaddr, chaddr, drop=False):
 
 
 def _get_address_v6(ia_na, duid, drop=False):
+    """
+    Helper function to get v6 lease or check for DROP
+    :param ia_na: desired ip address
+    :param duid: MAC address
+    :param drop: If request should be dropped by Kea
+    """
     misc.test_procedure()
     srv_msg.client_sets_value('Client', 'DUID', duid)
     srv_msg.client_does_include('Client', 'client-id')
@@ -78,9 +90,16 @@ def _get_address_v6(ia_na, duid, drop=False):
 @pytest.mark.v4
 @pytest.mark.v6
 @pytest.mark.host_reservation
-def test_host(dhcp_version):
+@pytest.mark.parametrize('backend', ['memfile', 'mysql', 'postgresql'])
+def test_early_hr_lookup_class(backend, dhcp_version):
+    """
+    Test to check if Kea looks up Global Host Reservations and assigns client class before subnet allocation.
+    After assigning client class, the subnet should be allocated according to class.
+    """
     misc.test_setup()
+    srv_control.define_temporary_lease_db_backend(backend)
 
+    # Define 2 subnets, with one guarded by class.
     subnets_v4 = [
         {
             'id': 1,
@@ -128,20 +147,18 @@ def test_host(dhcp_version):
         }
     ]
 
+    # Define client class to guard subnet.
     client_classes = [
         {
             'name': 'first'
         }
     ]
 
+    # Define host reservation that can access "first" subnet.
     reservations_v4 = [
         {
             'client-classes': ['first'],
             'hw-address': 'ff:01:02:03:ff:04'
-        },
-        {
-            'client-classes': ['DROP'],
-            'hw-address': 'ff:01:02:03:ff:05'
         }
     ]
     reservations_v6 = [
@@ -149,15 +166,12 @@ def test_host(dhcp_version):
             'client-classes': ['first'],
             'duid': '00:03:00:01:f6:f5:f4:f3:f2:04'
         },
-        {
-            'client-classes': ['DROP'],
-            'duid': '00:03:00:01:f6:f5:f4:f3:f2:05'
-        }
     ]
-    # world.dhcp_cfg['reservations-global'] = True
-    # world.dhcp_cfg['reservations-in-subnet'] = False
+
+    # Enable Early Global Host Reservation Lookup.
     world.dhcp_cfg['early-global-reservations-lookup'] = True
 
+    # apply definitions to server configuration.
     if dhcp_version == 'v4':
         world.dhcp_cfg.update({'subnet4': copy.deepcopy(subnets_v4)})
         world.dhcp_cfg.update({'reservations': copy.deepcopy(reservations_v4)})
@@ -169,11 +183,83 @@ def test_host(dhcp_version):
     srv_control.build_and_send_config_files()
     srv_control.start_srv('DHCP', 'started')
 
+    # Check if proper IP is assigned to MAC address.
     if dhcp_version == 'v4':
         _get_address_v4('192.168.50.1', 'ff:01:02:03:ff:04')
-        _get_address_v4(None, 'ff:01:02:03:ff:05', drop=True)
         _get_address_v4('192.168.51.1', 'ff:01:02:03:ff:06')
     else:
         _get_address_v6('2001:db8:1::1', '00:03:00:01:f6:f5:f4:f3:f2:04')
-        _get_address_v6(None, '00:03:00:01:f6:f5:f4:f3:f2:05', drop=True)
         _get_address_v6('2001:db8:2::1', '00:03:00:01:f6:f5:f4:f3:f2:06')
+
+
+@pytest.mark.v4
+@pytest.mark.v6
+@pytest.mark.host_reservation
+@pytest.mark.parametrize('backend', ['memfile', 'mysql', 'postgresql'])
+def test_early_hr_lookup_drop(backend, dhcp_version):
+    """
+    Test to check if Kea looks up Global Host Reservations and assigns DROP class.
+    """
+    misc.test_setup()
+    srv_control.define_temporary_lease_db_backend(backend)
+    # Define subnet.
+    subnets_v4 = [
+        {
+            'id': 1,
+            'interface': world.f_cfg.server_iface,
+            'pools': [
+                {
+                    'pool': '192.168.50.1-192.168.50.50'
+                }
+            ],
+            'subnet': '192.168.50.0/24',
+        }
+    ]
+    subnets_v6 = [
+        {
+            'id': 1,
+            'interface': world.f_cfg.server_iface,
+            'pools': [
+                {
+                    'pool': '2001:db8:1::1-2001:db8:1::50'
+                }
+            ],
+            'subnet': '2001:db8:1::/64',
+        }
+    ]
+
+    # Define host reservation that should be dropped.
+    reservations_v4 = [
+        {
+            'client-classes': ['DROP'],
+            'hw-address': 'ff:01:02:03:ff:05'
+        }
+    ]
+    reservations_v6 = [
+        {
+            'client-classes': ['DROP'],
+            'duid': '00:03:00:01:f6:f5:f4:f3:f2:05'
+        }
+    ]
+
+    # Enable Early Global Host Reservation Lookup.
+    world.dhcp_cfg['early-global-reservations-lookup'] = True
+
+    # Apply definitions to server configuration.
+    if dhcp_version == 'v4':
+        world.dhcp_cfg.update({'subnet4': copy.deepcopy(subnets_v4)})
+        world.dhcp_cfg.update({'reservations': copy.deepcopy(reservations_v4)})
+    else:
+        world.dhcp_cfg.update({'subnet6': copy.deepcopy(subnets_v6)})
+        world.dhcp_cfg.update({'reservations': copy.deepcopy(reservations_v6)})
+
+    srv_control.build_and_send_config_files()
+    srv_control.start_srv('DHCP', 'started')
+
+    # Check if classless MAC gets IP and reserved host is dropped.
+    if dhcp_version == 'v4':
+        _get_address_v4('192.168.50.1', 'ff:01:02:03:ff:04')
+        _get_address_v4(None, 'ff:01:02:03:ff:05', drop=True)
+    else:
+        _get_address_v6('2001:db8:1::1', '00:03:00:01:f6:f5:f4:f3:f2:04')
+        _get_address_v6(None, '00:03:00:01:f6:f5:f4:f3:f2:05', drop=True)
