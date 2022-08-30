7b611c812157a19249e4e2d805d0d15414cc4fb8
Wlodek Wencel <wlodek@isc.org>
2022-08-30 16:50:22 +0200

    removed pgsql config change

--- vagrant/provision-srv-rpm.sh
+++ vagrant/provision-srv-rpm.sh
@@ -6,9 +6,5 @@ sudo dnf install -y socat freeradius bind net-tools tcpdump
 # this is needed for ddns tests
 sudo ip -6 route add 2001:db8:1::/64 dev enp0s9
 
-# enable access to pgsql
-sudo sed -i -e s/ident/md5/ /var/lib/pgsql/data/pg_hba.conf
-sudo systemctl restart postgresql
-
 # Generate certificates for the FreeRADIUS server.
 sudo /etc/raddb/certs/bootstrap
