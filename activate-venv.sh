6ec55dac4822e2e1d83df8af8147e56063dc62b6
Marcin Godzina <mgodzina@isc.org>
2022-11-25 10:38:28 +0000

    moved pylint instalation to `activate-venv.sh`

--- activate-venv.sh
+++ activate-venv.sh
@@ -8,6 +8,7 @@ if test ! -d ./venv; then
   python3 -m venv ./venv
   ./venv/bin/pip install --upgrade pip
   ./venv/bin/pip install -r requirements.txt
+  ./venv/bin/pip install pylint==2.15.5 pycodestyle==2.9.0
 fi
 
 . ./venv/bin/activate
