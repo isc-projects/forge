ce2992beaeba97c85d036b0f01558440994d744b
Wlodek Wencel <wlodek@isc.org>
2023-03-22 17:57:51 +0100

    small tweaks for alpine support

--- vagrant/provision-srv-apk.sh
+++ vagrant/provision-srv-apk.sh
@@ -2,9 +2,8 @@
 set -e -x
 
 sudo apk update
-sudo apk add -y socat gnupg  net-tools tcpdump
-
-#freeradius bind9
+sudo apk add socat gnupg bind net-tools tcpdump
+#TODO if we will provide freeradius pkgs for alpine, add it here
 
 # this is needed for ddns tests
 sudo ip -6 route add 2001:db8:1::/64 dev enp0s9
