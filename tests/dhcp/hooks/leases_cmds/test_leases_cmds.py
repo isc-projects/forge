711b088e3d57368958f036b5b6a2b7e9c38cd2a9
Marcin Godzina <mgodzina@isc.org>
2023-05-18 12:21:37 +0200

    pylint fix

--- tests/dhcp/hooks/leases_cmds/test_leases_cmds.py
+++ tests/dhcp/hooks/leases_cmds/test_leases_cmds.py
@@ -16,7 +16,7 @@ from src import srv_msg
 from src import srv_control
 from src.protosupport.dhcp4_scen import DHCPv6_STATUS_CODES
 from src.softwaresupport.multi_server_functions import  fabric_sudo_command
-from src.protosupport.multi_protocol_functions import file_contains_line, file_doesnt_contain_line, sort_container
+from src.protosupport.multi_protocol_functions import file_contains_line, sort_container
 from src.forge_cfg import world
 
 
