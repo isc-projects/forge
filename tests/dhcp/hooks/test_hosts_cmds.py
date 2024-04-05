8f9fd1bf56f8fc637043bd9249638d528f99fc6c
Marcin Godzina <mgodzina@isc.org>
2024-04-05 10:38:53 +0200

    res-del-testing

--- tests/dhcp/hooks/test_hosts_cmds.py
+++ tests/dhcp/hooks/test_hosts_cmds.py
@@ -2280,6 +2280,7 @@ def test_v6_del_reservation(channel, host_database, query_type):
     srv_control.add_hooks('libdhcp_host_cmds.so')
     srv_control.config_srv_subnet('2001:db8:1::/64', '2001:db8:1::50-2001:db8:1::60')
     srv_control.config_srv_prefix('3000:db8::', 0, 32, 34)
+    srv_control.config_srv_prefix('3000:db8::', 0, 32, 34)
     srv_control.open_control_channel()
     if channel == 'http':
         srv_control.agent_control_channel()
@@ -2290,11 +2291,15 @@ def test_v6_del_reservation(channel, host_database, query_type):
     srv_control.start_srv('DHCP', 'started')
 
     srv_msg.SARR('2001:db8:1::50', '3000:db8::/34')
+    srv_msg.SARR('2001:db8:1::50', '3000:db8::/34')
 
     res = [{
         "duid": "00:03:00:01:f6:f5:f4:f3:f2:01",
         "ip-addresses": [
             "2001:db8:1::101"
+        ],
+        "prefixes": [
+            "3000:db8:1:0:1000::/110"
         ],
         "prefixes": [
             "3000:db8:1:0:1000::/110"
@@ -2308,11 +2313,19 @@ def test_v6_del_reservation(channel, host_database, query_type):
         "prefixes": [
             "3000:db8:2:0:1000::/110"
         ],
+         ],
+        "prefixes": [
+            "3000:db8:2:0:1000::/110"
+        ],
          "subnet-id": 1
          },
         {"duid": "00:03:00:01:f6:f5:f4:f3:f2:03",
          "ip-addresses": [
                  "2001:db8:1::103"
+         ],
+        "prefixes": [
+            "3000:db8:3:0:1000::/110"
+        ],
          ],
         "prefixes": [
             "3000:db8:3:0:1000::/110"
@@ -2326,6 +2339,10 @@ def test_v6_del_reservation(channel, host_database, query_type):
         "prefixes": [
             "3000:db8:4:0:1000::/110"
         ],
+         ],
+        "prefixes": [
+            "3000:db8:4:0:1000::/110"
+        ],
          "subnet-id": 1
          }]
 
@@ -2333,7 +2350,7 @@ def test_v6_del_reservation(channel, host_database, query_type):
         _reservation_add(reservation, target=_get_target(host_database), channel=channel)
 
     srv_msg.SARR('2001:db8:1::101', '3000:db8:1:0:1000::/110')
-    _get_multiple_iana(['2001:db8:1::104', '2001:db8:1::105'], [2123, 2124], '00:03:00:01:f6:f5:f4:f3:f2:04')
+    _get_multiple_iana(['2001:db8:1::104','2001:db8:1::105'], [2123,2124], '00:03:00:01:f6:f5:f4:f3:f2:04')
 
     del_res = {
         "ip-address": "2001:db8:1::101",
@@ -2360,7 +2377,7 @@ def test_v6_del_reservation(channel, host_database, query_type):
     assert response["text"] == "3 IPv6 host(s) found."
 
     srv_msg.SARR('2001:db8:1::51', '3000:db8:4000::/34')
-    _get_multiple_iana(['2001:db8:1::104', '2001:db8:1::105'], [2123, 2124],  '00:03:00:01:f6:f5:f4:f3:f2:04')
+    _get_multiple_iana(['2001:db8:1::104','2001:db8:1::105'], [2123,2124],  '00:03:00:01:f6:f5:f4:f3:f2:04')
 
     del_res = {
         "ip-address": "2001:db8:1::105",
@@ -2386,6 +2403,8 @@ def test_v6_del_reservation(channel, host_database, query_type):
     assert response["result"] == 0
     assert response["text"] == "2 IPv6 host(s) found."
 
+    srv_msg.SARR('2001:db8:1::52', '3000:db8:4000::/34')
+    srv_msg.SARR('2001:db8:1::53', '3000:db8:8000::/34', duid='00:03:00:01:f6:f5:f4:f3:f2:04')
     srv_msg.SARR('2001:db8:1::52', '3000:db8:4000::/34')
     srv_msg.SARR('2001:db8:1::53', '3000:db8:8000::/34', duid='00:03:00:01:f6:f5:f4:f3:f2:04')
 
