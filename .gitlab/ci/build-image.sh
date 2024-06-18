5a7fc1a9c9701b01cbf1aa2468671cf28ca4b189
Wlodek Wencel <wlodek@isc.org>
2024-06-18 13:26:44 +0200

    keep just one requirements.txt file

--- .gitlab/ci/build-image.sh
+++ .gitlab/ci/build-image.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
+cp ../../requirements.txt .
 docker login registry.gitlab.isc.org
 docker build --no-cache -t registry.gitlab.isc.org/isc-projects/forge ./
 docker push registry.gitlab.isc.org/isc-projects/forge
