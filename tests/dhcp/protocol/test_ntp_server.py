43a644aba77ec81f58d5bbc814c9d4e579605e18
Marcin Godzina <mgodzina@isc.org>
2025-07-30 12:39:45 +0200

    [#525] ntp negative test

--- tests/dhcp/protocol/test_ntp_server.py
+++ tests/dhcp/protocol/test_ntp_server.py
@@ -85,48 +85,107 @@ def test_v6_ntp_negative():
     """
     Check if Kea discards wrong configuration.
     """
+    # Static part of option data configuration.
     option_data_template = [{
         "always-send": True,
         "code": 56,
         "name": "ntp-server",
         "space": "dhcp6"}]
 
+    # Test cases of wrong configuration with expected error message.
     test_cases = [
-        {'option_data': {
-            "space": "v6-ntp-server-suboptions",
-            "name": "ntp-server",
-            "data": "2001:db8:abcd::123"
-        },
+        {  # wrong option name
+            'option_data': {
+                "space": "v6-ntp-server-suboptions",
+                "name": "ntp-server",
+                "data": "2001:db8:abcd::123"
+            },
             'log_message': 'definition for the option \'v6-ntp-server-suboptions.ntp-server\' '
             'having code \'0\' does not exist'
         },
-        {'option_data': {
-            "space": "v6-ntp-server-suboptions",
-            "name": "ntp-server-address",
-            "data": "lorem ipsum"
-        },
+        {  # wrong address in ntp-server-address
+            'option_data': {
+                "space": "v6-ntp-server-suboptions",
+                "name": "ntp-server-address",
+                "data": "lorem ipsum"
+            },
             'log_message': 'Failed to convert string to address \'lorem ipsum\': Invalid argument'
         },
-        {'option_data': {
-            "space": "v6-ntp-server-suboptions",
-            "name": "ntp-server-address",
-            "data": ""
+        {  # empty address in ntp-server-address
+            'option_data': {
+                "space": "v6-ntp-server-suboptions",
+                "name": "ntp-server-address",
+                "data": ""
+            },
+            'log_message': 'option data does not match option definition '
+            '(space: v6-ntp-server-suboptions, code: 1): no option value specified'
         },
-            'log_message': 'Failed to convert string to address \'\': Invalid argument'
+        {  # wrong address in ntp-server-address
+            'option_data': {
+                "space": "v6-ntp-server-suboptions",
+                "name": "ntp-server-address",
+                "data": "2001:db8::abcd::123"
+            },
+            'log_message': 'Failed to convert string to address \'2001:db8::abcd::123\': Invalid argument'
+        },
+        {  # wrong address in ntp-server-multicast
+            'option_data': {
+                "space": "v6-ntp-server-suboptions",
+                "name": "ntp-server-multicast",
+                "data": "lorem ipsum"
+            },
+            'log_message': 'Failed to convert string to address \'lorem ipsum\': Invalid argument'
+        },
+        {  # empty address in ntp-server-multicast
+            'option_data': {
+                "space": "v6-ntp-server-suboptions",
+                "name": "ntp-server-multicast",
+                "data": ""
+            },
+            'log_message': 'option data does not match option definition '
+            '(space: v6-ntp-server-suboptions, code: 2): no option value specified'
+        },
+        {  # wrong address in ntp-server-multicast
+            'option_data': {
+                "space": "v6-ntp-server-suboptions",
+                "name": "ntp-server-multicast",
+                "data": "2001:db8::abcd::123"
+            },
+            'log_message': 'Failed to convert string to address \'2001:db8::abcd::123\': Invalid argument'
+        },
+        {  # wrong fqdn in ntp-server-fqdn
+            'option_data': {
+                "space": "v6-ntp-server-suboptions",
+                "name": "ntp-server-fqdn",
+                "data": "lorem ipsum"
+            },
+            'log_message': 'option data does not match option definition '
+            '(space: v6-ntp-server-suboptions, code: 3): option buffer truncated'
+        },
+        {  # empty address in ntp-server-fqdn
+            'option_data': {
+                "space": "v6-ntp-server-suboptions",
+                "name": "ntp-server-fqdn",
+                "data": ""
+            },
+            'log_message': 'option data does not match option definition '
+            '(space: v6-ntp-server-suboptions, code: 3): no option value specified'
         }
     ]
 
     for test_case in test_cases:
-        ic(test_case)
+        # Clear logs and setup test environment.
         srv_control.clear_some_data('logs')
         misc.test_setup()
-        srv_control.config_srv_subnet(
-            '2001:db8:a::/64', '2001:db8:a::1-2001:db8:a::10')
-        ic(world.dhcp_cfg['option-data'])
-        ic(option_data_template)
-        world.dhcp_cfg['option-data'] = option_data_template
-        ic(world.dhcp_cfg['option-data'])
+        srv_control.config_srv_subnet('2001:db8:a::/64', '2001:db8:a::1-2001:db8:a::10')
+
+        # Add static option data to the configuration.
+        world.dhcp_cfg['option-data'] = option_data_template.copy()
+        # Add test case specific option data to the configuration.
         world.dhcp_cfg['option-data'].append(test_case['option_data'])
+        # Print test case in green for debugging.
+        print(f'\033[92mOption test case: {test_case["option_data"]}\033[0m')
+
         srv_control.build_and_send_config_files()
         srv_control.start_srv('DHCP', 'started', should_succeed=False)
 
