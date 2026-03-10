49c08b8f0460834d599adf5e681ce4d7e3971414
Andrei Pavel <andrei@isc.org>
2026-03-10 15:49:44 +0200

    [#595] Add test_kea_4365_crash_empty_control_sockets.py

---
+++ tests/security/test_kea_4365_crash_empty_control_sockets.py
@@ -0,0 +1,31 @@
+# Copyright (C) 2026 Internet Systems Consortium, Inc. ("ISC")
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import pytest
+
+from src import srv_msg
+from src import srv_control
+from src import misc
+
+from src.forge_cfg import world
+
+
+@pytest.mark.controlchannel
+def test_kea_4365_empty_control_sockets():
+    """kea#4365"""
+    if not world.f_cfg.control_agent:
+        pytest.skip('This test only makes sense for CA.')
+
+    misc.test_setup()
+    srv_control.add_http_control_channel()
+    srv_control.build_and_send_config_files()
+    srv_control.start_srv('DHCP', 'started')
+
+    config = {'Control-agent': {'control-sockets': {'': {}}}}
+    command = {'command': 'config-set', 'arguments': config}
+
+    # Expect error.
+    srv_msg.send_ctrl_cmd_via_http(command, exp_result=1)
