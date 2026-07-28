b22cd86e0a003291aff49c04af0423a5771811bf
Wlodek Wencel <wlodek@isc.org>
2026-07-28 17:03:08 +0200

    [#463] temp change to remove netconf

--- incus.sh
+++ incus.sh
@@ -588,7 +588,7 @@ function install_kea_tarball() {
         log "Installing kea from the source code on node kea-$node - $usedSystem $osVersion"
         incus exec kea-"$node" -- rm -rf /tmp/kea
         incus file push -r -q "$2" kea-"$node"/tmp/.
-        incus exec kea-"$node" --cwd=/tmp/kea -- python3 /tmp/hammer.py build -p local -w ccache,forge,install,mysql,pgsql,shell,gssapi,netconf -x docs,perfdhcp,unittest --ccache-dir /ccache #
+        incus exec kea-"$node" --cwd=/tmp/kea -- python3 /tmp/hammer.py build -p local -w ccache,forge,install,mysql,pgsql,shell,gssapi -x docs,perfdhcp,unittest --ccache-dir /ccache #
     done
     printf '\nINSTALL_METHOD="make"\n' >> install_method
 }
